9de776c6e50b99bda058a7b1449f94a1e0e4b8ac
soomok-lee <soomok.us@gmail.com>
2020-12-29 20:17:42 -0800

    tdd test, get.

--- index.js
+++ index.js
@@ -11,7 +11,21 @@ var users = [
 ];
 
 app.get('/users', function(req, res) {
-    res.json(users);
+    req.query.limit = req.query.limit || 10;
+    const limit = parseInt(req.query.limit, 10);
+    if(Number.isNaN(limit)) return res.status(400).end();
+    
+    res.json(users.slice(0, limit));
+});
+
+app.get('/users/:id', function(req, res) {
+   const id = parseInt(req.params.id, 10);
+   if(Number.isNaN(id)) return res.status(400).end();
+    
+   const user = users.filter((user)=> user.id === id)[0];
+   if(!user) return res.status(404).end();
+
+   res.json(user);
 });
 
 app.listen(3000, function() {
--- index.spec.js
+++ index.spec.js
@@ -1,21 +1,3 @@
-const app = require('./index');
-const request = require('supertest'); // npm i supertest --save-dev
-const should = require('should');
-
-describe('GET /users', ()=> {
-    describe('success, ', ()=> {
-        it('respond with an array of user objects.', (done)=> {
-            request(app)
-                .get('/users')
-                .end((err, res)=> {
-                    // console.log(res.body);
-                    res.body.should.be.instanceof(Array)
-                    done();
-                })    
-        })
-    })
-})
-
 /*
 // test script 등록
 // node_modules/.bin/mocha index.spec.js
@@ -29,3 +11,67 @@ describe('GET /users', ()=> {
 // npm test
 // npm start
 */
+const app = require('./index');
+const request = require('supertest'); // npm i supertest --save-dev
+const should = require('should');
+
+describe('GET /users', ()=> {
+    describe('success, ', ()=> {
+        it('response with an array of user objects.', (done)=> {
+            request(app)
+                .get('/users')
+                .end((err, res)=> {
+                    // console.log(res.body);
+                    res.body.should.be.instanceof(Array)
+                    done();
+                })    
+        });
+
+        it('response is within the limit.', (done)=> {
+            request(app)
+                .get('/users?limit=2')
+                .end((err, res)=> {
+                    res.body.should.have.lengthOf(2)
+                    done();
+                })    
+        });
+    });
+
+    describe('fail, ', ()=> {
+        it('response with the status code 400 if the limit is not Number.', (done)=> {
+            request(app)
+                .get('/users?limit=two')
+                .expect(400)
+                .end(done);
+        })
+    });
+});
+
+describe('GET /users/:id', ()=> {
+    describe('success, ', ()=> {
+        it('response with an user object with id 1.', (done)=> {
+            request(app)
+                .get('/users/1')
+                .end((err, res)=> {
+                    res.body.should.have.property('id', 1)
+                    done();
+                })    
+        });    
+    });
+
+    describe('fail, ', ()=> {
+        it('response with the status code 400 if the id is not Number.', (done)=> {
+            request(app)
+                .get('/users/one')
+                .expect(400)
+                .end(done);  
+        });   
+        
+        it('response with the status code 404 if the id is not found.', (done)=> {
+            request(app)
+                .get('/users/999')
+                .expect(404)
+                .end(done);  
+        });  
+    });
+});
