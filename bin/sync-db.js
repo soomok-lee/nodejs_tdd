da357cffe955bc858abb269e1ede928df99f8352
soomok-lee <soomok.us@gmail.com>
2021-01-05 11:27:01 -0800

    added db option.

--- bin/sync-db.js
+++ bin/sync-db.js
@@ -1,5 +1,9 @@
 const models = require('../models');
 
 module.exports = () => {
-    return models.sequelize.sync({force: true});
+    const options = {
+        // force option
+        force: process.env.NODE_ENV === 'test' ? true : false
+    }
+    return models.sequelize.sync(options);
 }
